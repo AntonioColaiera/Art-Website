928f976ac5e110eacd34b7ef0579f88fafc9ab1e
Antonio Colaierà <colaiera@gmail.com>
2023-10-02 11:26:38 +0200

    zoom card and menu

--- art-website/src/component/Card.js
+++ art-website/src/component/Card.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './Card.css';
 
 function Card(props) {
   return (
-    <div className="col-md-3 ">
-      <div className="card bg-dark text-white mb-4">
+    <div className="col-md-3">
+      <div className="card bg-dark text-white mb-4 card-zoom">
         <img src={props.imageSrc} className="card-img" alt={props.imageAlt} />
         <div className="card-img-overlay">
           <h5 className="card-title text-center">{props.title}</h5>
@@ -12,7 +13,5 @@ function Card(props) {
     </div>
   );
 }
-
-
 
 export default Card;
--- art-website/src/component/Navbar.js
+++ art-website/src/component/Navbar.js
@@ -1,32 +1,53 @@
-import React from 'react';
-import { FaInstagram } from 'react-icons/fa';
+import React from 'react'
+import { FaInstagram } from 'react-icons/fa'
 
-
-
-function Navbar(){
-    return (
-      <div>
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class="container-fluid">
-   <div class="navbar-brand">Vulitivibeni<span style={{ color: '#D81B60', fontStyle: 'italic' }}>ART</span></div>
-        <button type="button" class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarCollapse">
-            <div class="navbar-nav ms-auto align-items-center">
-            <a href="/" class="nav-item nav-link active mx-2">Home</a>
-            <a href="/about" class="nav-item nav-link mx-2">About</a> 
-            <a href="https://www.instagram.com/vulitivibenimore/" target="_blank" rel="noopener noreferrer">
-            <FaInstagram className="fa-lg mx-2" style={{ fontSize: '1.5em' }} />
-            </a>
-            <a href="mailto:colaiera@email.com"  class="nav-item nav-link mx-2">Get in touch</a>
+function Navbar() {
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid">
+          <div className="navbar-brand">
+            Vulitivibeni
+            <span style={{ color: '#D81B60', fontStyle: 'italic' }}>ART</span>
+          </div>
+          <button
+            type="button"
+            className="navbar-toggler"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarCollapse"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarCollapse">
+            <div className="navbar-nav ms-auto align-items-center">
+              <a href="/" className="nav-item nav-link active mx-2">
+                Home
+              </a>
+              <a href="/about" className="nav-item nav-link mx-2">
+                About
+              </a>
+              <a
+                href="https://www.instagram.com/vulitivibeniart/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaInstagram
+                  className="fa-lg mx-2"
+                  style={{ fontSize: '1.5em' }}
+                />
+              </a>
+              <a
+                href="mailto:colaiera@email.com"
+                className="nav-item nav-link mx-2"
+              >
+                Get in touch
+              </a>
             </div>
+          </div>
         </div>
+      </nav>
     </div>
-</nav>
-      </div>
-    );
-  }
-  
-  export default Navbar
+  )
+}
 
+export default Navbar
