8d36957969606ae66246ef2825ea27a01e867fba
Antonio Colaierà <colaiera@gmail.com>
2023-11-03 15:30:22 +0100

    About color

--- art-website/src/component/About.js
+++ art-website/src/component/About.js
@@ -1,6 +1,18 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 
 function About() {
+  const [colors, setColors] = useState(["red", "blue", "green"]);
+
+  useEffect(() => {
+    const generateRandomColors = () => {
+      const availableColors = ["red", "blue", "green", "yellow", "orange", "purple"];
+      const randomColors = availableColors.slice(0, 3);
+      setColors(randomColors);
+    };
+
+    generateRandomColors(); // Chiamare la funzione di generazione dei colori al montaggio iniziale.
+  }, []);
+
   return (
     <div
       className="d-flex justify-content-center align-items-center"
@@ -10,21 +22,24 @@ function About() {
         style={{
           width: '900px',
           height: '300px',
-          backgroundColor: 'lightgray',
+          background: `linear-gradient(to right, ${colors[0]}, ${colors[1]}, ${colors[2]})`,
           border: '2px solid #000000',
           padding: '20px',
           borderRadius: '15px',
-          fontStyle: 'italic'
+          fontStyle: 'italic',
         }}
       >
-        <h1>
+        <h1 className='text-white'>
           This simple collection of paintings expresses the desire to create,
           albeit at an amateur level. Painting is something wonderful that
           allows you to dilate time until it is erased.
         </h1>
       </div>
     </div>
-  )
+  );
 }
 
-export default About
+export default About;
+
+
+
